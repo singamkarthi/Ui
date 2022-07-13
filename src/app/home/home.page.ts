7479b288d06d5b3339c8c41491a3497860898998
Brabu Kannan <brabu.yoventech@gmail.com>
2022-07-13 13:18:15 +0530

    menu try

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public navCtrl : NavController) {}
 
+  goto(view){
+    this.navCtrl.navigateForward(view);
+  }
 }
